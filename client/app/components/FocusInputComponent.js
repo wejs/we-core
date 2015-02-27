fb34cf76d7572d885ce81ff56b9fff8570d599e9
Sávio Lucena <saviogl@gmail.com>
2015-02-27 16:53:25 -0300

    enhance with countable property

--- client/app/components/FocusInputComponent.js
+++ client/app/components/FocusInputComponent.js
@@ -9,12 +9,35 @@
  */
 
 App.FocusInputComponent = Ember.TextField.extend({
-  didInsertElement: function didInsertElement() {
+	init: function () {
+		// body...
+		this._super.apply(this, arguments);
+
+		if (this.get('type') != 'checkbox' && this.get('limit')) {
+			this.addObserver('value', this, this.countCharacters);			
+		}
+	},
+
+	didInsertElement: function () {
+		this.countCharacters();
     this.$().focus(function onFocusInput() {
       // move the cursor to end of editing text
       this.selectionStart = this.selectionEnd = this.value.length;
     });
     // trigger the focus
-    this.$().focus();
-  }
+    this.$().focus();		
+	},
+
+	countCharacters: function () {
+		// body...
+		if (!this.get('value')) {
+			return this.set('trackCount', parseInt(this.get('limit')));
+		}
+		var diff = parseInt(this.get('limit')) - this.get('value').length;
+		if (diff < 0) {
+			this.set('value', this.get('value').substr(0, parseInt(this.get('limit'))))
+			return this.set('trackCount', 0);
+		}
+		return this.set('trackCount', diff);
+	}  
 });
