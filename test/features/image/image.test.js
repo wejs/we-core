92ba8c5d67a03573e6dc649c6ea36e7dd8db7874
albertosouza <alberto.souza.99@gmail.com>
2015-03-08 20:53:34 -0300

    add create and findOne image tests

--- test/features/image/image.test.js
+++ test/features/image/image.test.js
@@ -2,30 +2,43 @@ var assert = require('assert');
 var request = require('supertest');
 var helpers = require('../../helpers');
 var stubs = require('../../stubs');
+var _ = require('lodash');
 var http;
 
 describe('imageFeature', function () {
-  before(function (cb) {
-    http = helpers.getHttp();
+  var salvedImage;
 
-    cb();
+  before(function (done) {
+    http = helpers.getHttp();
+    // upload one stub image:
+    request(http)
+    .post('/api/v1/image')
+    .attach('image', stubs.getImageFilePath())
+    .end(function (err, res) {
+      salvedImage = res.body.image[0];
+      done(err);
+    });
   }); 
 
-  // describe('find', function () {
-  //   it('get /api/v1/image route should exists', function(done){ 
-  //     request(http)
-  //     .get('/api/v1/image')
-  //     .end(function (err, res) {
-  //       assert.equal(200, res.status);
-  //       assert(res.body.image);
-  //       assert(res.body.meta);
-
-  //       done();
-  //     });
-  //   });
-  // })
+  describe('find', function () {
+    it('get /api/v1/image route should find one image', function(done){ 
+      request(http)
+      .get('/api/v1/image')
+      .end(function (err, res) {
+        assert.equal(200, res.status);
+        assert(res.body.image);
+        assert( _.isArray(res.body.image) , 'image not is array');
+        assert.equal(res.body.image[0].id, salvedImage.id, 'first image returned from find dont are the salved image');
+        assert(res.body.meta);
+        done();
+      });
+    });
+  });
 
   describe('create', function () {
+    // file upload and creation is slow then set to 300
+    this.slow(300);
+
     it('post /api/v1/image create one image record', function(done){ 
       request(http)
       .post('/api/v1/image')
@@ -33,8 +46,32 @@ describe('imageFeature', function () {
       .end(function (err, res) {
         assert.equal(201, res.status);
         assert(res.body.image);
+        assert(res.body.image[0].mime);
+        assert(res.body.image[0].width);
+        assert(res.body.image[0].height);
+
         done();
       });
     });
   });  
+
+  describe('findOne', function () {
+    it('get /api/v1/image/:name should return one image file', function(done){ 
+      request(http)
+      .get('/api/v1/image/' + salvedImage.name)
+      .attach('image', stubs.getImageFilePath())
+      .end(function (err, res) {
+
+        assert.equal(200, res.status);
+        assert.equal(res.type, salvedImage.mime);
+        assert(res.body);
+
+        done();
+      });
+    });
+  });
+ 
+  describe('remove', function () {
+    it('delete /api/v1/image/:name should delete one image file');
+  });
 });
