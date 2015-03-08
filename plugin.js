518680fd805f5af5e1c994d3cad34d6f6a7f0851
albertosouza <alberto.souza.99@gmail.com>
2015-03-08 17:25:35 -0300

    update image upload feature and configs

--- plugin.js
+++ plugin.js
@@ -2,8 +2,21 @@
  * We.js plugin config
  */
 
+var uuid = require('node-uuid');
+var log = require('./lib/log')();
+
 module.exports = function loadPlugin(projectPath, Plugin) {
   var plugin = new Plugin(__dirname);
+
+  var imageMimeTypes = [
+    'image/png', 
+    'image/jpg', 
+    'image/jpeg', 
+    'image/gif', 
+    'image/bmp', 
+    'image/x-icon',
+    'image/tiff'
+  ];
 
   // set plugin configs
   plugin.setConfigs({
@@ -12,10 +25,12 @@ module.exports = function loadPlugin(projectPath, Plugin) {
     log: {
       level: 'debug'
     },
-    upload: {
-      dest: projectPath + '/files/tmp',
-      rename: function (fieldname, filename) {
-        return filename.replace(/\W+/g, '-').toLowerCase() + Date.now()
+    upload: { 
+      file: {
+        dest: projectPath + '/files/uploads/files',
+        rename: function (fieldname, filename) {
+          return Date.now() + '_' + uuid.v1();
+        }
       }
     },
     session: {
@@ -68,6 +83,79 @@ module.exports = function loadPlugin(projectPath, Plugin) {
     'get /api/v1/models/emberjs': {
       controller: 'main',
       action: 'getAllModelsAsEmberModel'
+    },
+
+    //
+    // --  Images routes
+    //
+    'get /api/v1/image': {
+      controller    : 'image',
+      action        : 'find',
+      model         : 'image'
+    },
+    'get /api/v1/image/:name?': {
+      controller    : 'image',
+      action        : 'findOne',
+      model         : 'image'
+    },
+    // Image style thumbnail | medium | large
+    'get /api/v1/image/:style(original|mini|thumbnail|medium|large)/:name': {
+      controller    : 'image',
+      action        : 'findOne',
+      model         : 'image'
+    },
+    'get /api/v1/image/:id/data': {
+      controller    : 'image',
+      action        : 'findOneReturnData',
+      model         : 'image'
+    },
+    'get /api/v1/image-crop/:id': {
+      controller    : 'image',
+      action        : 'cropImage',
+      model         : 'image'
+    },
+    'post /api/v1/image-crop/:id': {
+      controller    : 'image',
+      action        : 'cropImage',
+      model         : 'image'
+    },
+    // upload one image
+    'post /api/v1/image': {
+      controller    : 'image',
+      action        : 'create',
+      model         : 'image',
+      upload: {
+        dest: projectPath + '/files/uploads/images/original',
+        rename: function (fieldname, filename) {
+          return Date.now() + '_' + uuid.v1();
+        },
+        limits: {
+          fieldNameSize: 150,
+          files: 1,
+          fileSize: 10*1000000, // 10MB
+          fieldSize: 20*1000000 // 20MB
+        },        
+        onFileUploadStart: function(file) {
+          // check if file is valir on upload start
+          if (imageMimeTypes.indexOf(file.mimetype) < 0) {
+            log.debug('Image:onFileUploadStart: Invalid file type for file:', file);
+            // cancel upload on invalid type
+            return false;    
+          }
+        }        
+      }
+    },
+
+    // -- FILES
+
+    'get /files': {
+      controller    : 'files',
+      action        : 'find'
+    },
+
+    'post /files': {
+      controller    : 'files',
+      action        : 'create'
     },
 
     //
