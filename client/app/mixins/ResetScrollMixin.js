b3f8f1410e0ed4ed15cb10a9dc5d3d367f099def
albertosouza <alberto.souza.99@gmail.com>
2015-04-05 23:56:48 -0300

    update reset scroll logic

--- client/app/mixins/ResetScrollMixin.js
+++ client/app/mixins/ResetScrollMixin.js
@@ -3,8 +3,9 @@
  * Add this on one route to scroll to top every time enter in the route
  */
 App.ResetScrollMixin = Ember.Mixin.create({
-  activate: function() {
-    this._super();
-    window.scrollTo(0,0);
+  actions: {
+    didTransition: function() {
+      window.scrollTo(0,0);
+    }
   }
 });
