c200d4b8226d346da0019268c211f3d0426f8875
albertosouza <alberto.souza.99@gmail.com>
2015-04-13 21:47:41 -0300

    fix we.js test bootstrap and add we.bootstrapStarted flag

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,20 +1,22 @@
 var projectPath = process.cwd();
-var we = require('../lib');
 var deleteDir = require('rimraf');
 var fs = require('fs-extra');
 var path = require('path');
 var async = require('async');
+var we;
 
 before(function(callback) {
   this.slow(100);
 
   copyLocalConfigIfNotExitst(function() {
+    we = require('../lib');
     we.bootstrap({
       i18n: {
         directory: path.join(__dirname, 'locales'),
         updateFiles: true
       }
     } , function(err, we) {
+      if (err) return console.error(err);
       we.startServer(function(err) {
         if (err) return console.error(err);
         callback();
@@ -55,19 +57,17 @@ function copyLocalConfigIfNotExitst (cb) {
   var dest = path.resolve(projectPath, 'config', 'local.js');
 
   fs.lstat(dest, function(err) {
-    if (err) {
-      fs.ensureDir(path.resolve(projectPath, 'config'), function (err) {
+    if (!err) return cb();
+
+    fs.ensureDir(path.resolve(projectPath, 'config'), function (err) {
+      if (err) throw new Error(err);
+
+      var source = path.resolve(projectPath ,'test/stubs', 'local.js');
+      return fs.copy(source, dest, function(err) {
         if (err) throw new Error(err);
 
-        var source = path.resolve(projectPath ,'test/stubs', 'local.js');
-        return fs.copy(source, dest, function(err) {
-          if (err) throw new Error(err);
-
-          cb();
-        });
-
+        cb();
       });
-    }
-    cb();
+    });
   });
 }
