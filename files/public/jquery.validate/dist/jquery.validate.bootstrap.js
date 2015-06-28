049e052ce2b87cd0356aa7fa609a5a689bf73be1
albertosouza <alberto.souza.99@gmail.com>
2015-06-28 03:46:12 -0300

    update jquery validation localization files and add project version in assets

---
+++ files/public/jquery.validate/dist/jquery.validate.bootstrap.js
@@ -0,0 +1,19 @@
+// override jquery validate plugin defaults
+// get from http://stackoverflow.com/questions/18754020/bootstrap-3-with-jquery-validation-plugin
+$.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
