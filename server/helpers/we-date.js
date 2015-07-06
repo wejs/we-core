e1159eec19e060b58e16acdbb924998ed5d12af4
albertosouza <alberto.souza.99@gmail.com>
2015-07-06 02:10:26 -0300

    update we-date localization

--- server/helpers/we-date.js
+++ server/helpers/we-date.js
@@ -8,11 +8,12 @@ var moment = require('moment');
 module.exports = function(we) {
   return function renderDate(date, format) {
     if (!date) return '';
+    var options = arguments[arguments.length-1];
 
     var d = moment(date);
     if (!d.isValid()) return '';
 
-    var req = this.req;
+    var req = options.data.root.req;
     if (!req) req = this.locals.req;
     if (req && req.user) d.locale(req.user.language);
 
