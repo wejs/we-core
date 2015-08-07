3d3e8d010ee7bef2bf8d100fa1aa6bb5dfc31030
albertosouza <alberto.souza.99@gmail.com>
2015-08-07 00:17:21 -0300

    update t helper

--- server/helpers/t.js
+++ server/helpers/t.js
@@ -1,27 +1,34 @@
 /**
  * String localization with node i18n
  *
- * usage:  {{t 'string'}}
+ * usage:  {{t 'string' attr1='val1' attr2='val2'}}
  *
- * @return {[type]} [description]
+ * @return {string}
  */
 module.exports = function(we) {
-  return function t(string, attr) {
+  return function t(string) {
+    var options = arguments[arguments.length-1];
+    var attr, __;
+
+    if (arguments.length == 3) attr = arguments[2];
+
+    if (this.__) {
+      __ = this.__;
+    } else if (this.locals && this.locals.__) {
+      __ = this.locals.__;
+    } else if (options.data.root.locals && options.data.root.locals.__) {
+      __ = options.data.root.locals.__;
+    } else {
+        we.log.verbose('helper:t: this.__ not found, i will use we.i18n__');
+        console.log('>>', this)
+      __ = we.i18n.__;
+    }
+
     if (!string) return '';
     if (typeof string === 'string') {
-      if (this.__) {
-        return this.__(string);
-      } else {
-        we.log.verbose('helper:t: this.__ not found, i will use we.i18n__')
-        return we.i18n.__(string);
-      }
+      return __(string, options.hash);
     } else {
-      if (this.__) {
-        return this.__(string[attr]);
-      } else {
-        we.log.verbose('helper:t: this.__ not found, i will use we.i18n__')
-        return we.i18n.__(string[attr]);
-      }
+      return __(string[attr], options.hash);
     }
   }
 }
