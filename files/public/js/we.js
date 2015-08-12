4a5c587e9af3ef2b9e72e7fd37f85674056bdd22
albertosouza <alberto.souza.99@gmail.com>
2015-08-11 21:50:14 -0300

    update summeredit to add suport to videos and upload images

--- files/public/js/we.js
+++ files/public/js/we.js
@@ -621,26 +621,46 @@ we.components = {
       medium: [
         ['font', ['bold', 'italic', 'underline', 'clear']],
         ['para', [ 'ul','ol', 'paragraph' ]], ['style', ['style']],
-        ['insert', ['link', 'picture', 'video']],
+        ['insert', ['link', 'picture', 'video', 'table']],
         ['misc', ['fullscreen', 'help']]
       ]
     },
     init: function(selector, style) {
       var element = $(selector);
       var cfg = {
-        lang: we.config.locale,
+        lang: window.WE_BOOTSTRAP_CONFIG.locale,
+        styleWithSpan: false,
+        shortcuts: false,
         height: element.attr('we-editor-height'),
-        minHeight: element.attr('we-editor-minheight'),
+        minHeight: element.attr('we-editor-minheight') || 50,
         maxHeight: element.attr('we-editor-maxheight'),
-        focus: element.attr('we-editor-focus')
+        focus: element.attr('we-editor-focus'),
+
+        onImageUpload: function onImageUpload(files) {
+          var data = new FormData();
+          data.append('image', files[0]);
+          $.ajax({
+            data: data,
+            type: 'POST',
+            url: '/api/v1/image',
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(r) {
+              var img =  $('<img>');
+              img.attr('src', r.image[0].urls.large);
+              element.summernote('insertNode', img[0]);
+            }
+          });
+        }
       };
       // - set we-editor flag to use in form submit
       element.attr('we-editor', 'true');
-
       // get style config from we-editor-style atribute
       if (!style) style = (element.attr('we-editor-style') || 'medium');
       // add editor toobar config if style not is full
-      if (style !== 'full') cfg.toolbar = we.components.editor.styles[style];
+      cfg.toolbar = we.components.editor.styles[style];
+console.log('>>', style, cfg)
 
       element.summernote(cfg);
     }
@@ -748,3 +768,4 @@ $(function(){
 });
 
 })(window, window.page);
+
