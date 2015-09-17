0e39c9a274e9b459a0d42b719896a7fcef01f511
albertosouza <alberto.souza.99@gmail.com>
2015-09-17 01:28:08 -0300

    add modulePluginManager tests

---
+++ test/features/modules/pluginManager.test.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+var path = require('path');
+var npmf = path.resolve(process.cwd(), 'node_modules');
+
+describe('modulePluginManager', function () {
+  var pluginManager;
+
+  before(function (done) {
+    pluginManager = require('../../../lib/pluginManager');
+    done();
+  });
+
+  it('pluginManager.isPlugin should return false for async and moment', function (done) {
+    assert.equal(false, pluginManager.isPlugin(npmf+'/async'));
+    assert.equal(false, pluginManager.isPlugin(npmf+'/moment'));
+    assert.equal(false, pluginManager.isPlugin('invalid string'));
+    done();
+  });
+
+  it('pluginManager.isPlugin should return true for we-plugin-form', function (done) {
+    assert.equal(true, pluginManager.isPlugin(npmf+'/we-plugin-form'));
+    done();
+  });
+
+  it('pluginManager.getPluginNames should return plugin names list', function (done) {
+    var names = pluginManager.getPluginNames();
+    assert(names.length > 1);
+    assert(names.indexOf('project')>-1);
+    assert(names.indexOf('we-plugin-form')>-1) ;
+    done();
+  });
+
+});
