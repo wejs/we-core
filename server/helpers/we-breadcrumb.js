328ddb268510f8bdd22b6cc848c17b2e5bf53d36
albertosouza <alberto.souza.99@gmail.com>
2015-11-05 13:00:09 -0200

    update breadcrumb api to add suport to function handlers

--- server/helpers/we-breadcrumb.js
+++ server/helpers/we-breadcrumb.js
@@ -7,16 +7,36 @@
 module.exports = function(we) {
   return function helper() {
     var options = arguments[arguments.length-1];
-    var html = '';
+    var html = '', links, i;
 
-    if (
-      options.hash.locals &&
-      typeof options.hash.locals.breadcrumb == 'function'
-    ) {
+    if (!options.hash.locals) return '';
+
+    if (typeof options.hash.locals.breadcrumb == 'string') {
+      if (!we.router.breadcrumb[options.hash.locals.breadcrumb])
+        return '';
+
       html += '<ol class="breadcrumb">';
 
-      var links = options.hash.locals.breadcrumb();
-      for (var i = 0; i < links.length; i++) {
+      links = we.router.breadcrumb[options.hash.locals.breadcrumb]
+        .bind(options.hash.locals)();
+
+      for (i = 0; i < links.length; i++) {
+        // set last link as active
+        if (i == links.length) {
+          html +=  '<li>';
+        } else {
+          html +=  '<li class="active">';
+        }
+
+        html += '<a href="'+links[i].href+'">'+links[i].text+'</a></li>';
+      }
+
+      html += '</ol>';
+    } else if (typeof options.hash.locals.breadcrumb == 'function') {
+      html += '<ol class="breadcrumb">';
+
+      links = options.hash.locals.breadcrumb();
+      for (i = 0; i < links.length; i++) {
         // set last link as active
         if (i == links.length) {
           html +=  '<li>';
