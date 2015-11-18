df0f4ac9b501fe10ab042be938f873d7803fa5ef
albertosouza <alberto.souza.99@gmail.com>
2015-11-18 03:17:33 -0200

    update metadata tags API

--- server/helpers/render-metadata-tags.js
+++ server/helpers/render-metadata-tags.js
@@ -4,53 +4,8 @@
  * {{{render-metadata-tags}}}
  */
 
-module.exports = function(we) {
+module.exports = function() {
   return function renderMetadataTags() {
-    var metatags = '';
-
-    metatags += '<meta property="og:site_name" content="'+ we.config.appName +'">';
-
-    if (this.title) {
-      metatags += '<meta property="og:title" content="'+ this.title +'">';
-    }
-
-    if (this.metadata) {
-      if (this.metadata.keywords) {
-        metatags += '<meta name="keywords" content="'+ this.metadata.keywords +'">';
-      } else if( we.config.appKeywords) {
-        metatags += '<meta name="keywords" content="'+ we.config.appKeywords +'">';
-      }
-
-      if (this.metadata.description) {
-        metatags += '<meta name="description" content="'+ this.metadata.description +'">';
-        metatags += '<meta property="og:description" content="'+ this.metadata.description +'">';
-      }
-
-      if (this.metadata.image) {
-        metatags += '<meta property="og:image" content="'+ this.metadata.image +'">';
-      }
-
-      if (this.url) {
-        metatags += '<meta property="og:url" content="'+ this.url +'">';
-      }
-
-      if (this.metadata.type) {
-        metatags += '<meta property="og:type" content="'+ this.metadata.type +'" />';
-      }
-
-      // application metatags
-      metatags += '<meta property="application-name" content="'+ we.config.appName +'">';
-      metatags += '<meta property="application-url" content="'+ we.config.hostname +'">';
-
-    }
-
-    var evCTx = {
-      we: we,
-      metatags: metatags,
-      locals: this
-    };
-    we.events.emit('we-html-metadata', evCTx);
-
-    return evCTx.metatags;
+    return '<meta charset="UTF-8">'+this.metatag;
   }
 }
