3ff6f2e497ff39977e1627ab1f15d22af23f385c
albertosouza <alberto.souza.99@gmail.com>
2016-02-21 23:18:55 -0300

    add admin page to list installed themes

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,3 +1,5 @@
+var spawn = require('child_process').spawn;
+
 module.exports = {
   /**
    * Index page route /
@@ -5,5 +7,45 @@ module.exports = {
   index: function(req, res) {
     res.locals.template = 'home/index';
     res.view();
+  },
+
+  findThemes: function findThemes(req, res) {
+    var we = req.we;
+    we.utils.async.series([
+      // function (done) {
+      //   if (
+      //     !req.query.findNew ||
+      //     typeof req.query.findNew != 'string'
+      //   ) {
+      //     return done();
+      //   }
+
+      //   var opts = [
+      //     'info',
+      //     req.query.findNew
+      //   ];
+
+      //   var npmInfo = spawn('npm', opts);
+
+      //   res.locals.searchedThemeInfo = '';
+
+      //   npmInfo.stdout.on('data', function (data) {
+      //     res.locals.searchedThemeInfo += data;
+      //   });
+
+      //   npmInfo.on('close', function (code) {
+      //     if (code == 1) {
+      //       res.locals.searchedThemeNotFound = true;
+      //     }
+      //     done();
+      //   });
+      // }
+    ], function() {
+      res.locals.themes = we.view.themes;
+      res.locals.themeConfigs = we.config.themes;
+
+      res.ok();
+    });
+
   }
 }
