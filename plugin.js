209e5156b786d5fd4376528f0ebbf4a77ed286c8
albertosouza <alberto.souza.99@gmail.com>
2016-03-21 00:12:42 -0300

    add suport to configurable /robots.txt route and closes wejs/we#124

--- plugin.js
+++ plugin.js
@@ -35,6 +35,8 @@ module.exports = function loadPlugin(projectPath, Plugin) {
     appName: 'We.js app',
     appLogo: '/public/plugin/we-core/files/images/logo-small.png',
 
+    robotsTXT: __dirname + '/files/robots.txt',
+
     defaultUserAvatar: projectPath + '/node_modules/we-core/files/public/images/avatars/user-avatar.png',
 
     log: { level: 'debug' },
