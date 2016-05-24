2487b6e5bf2ae6952a039eaa775685bdae008603
albertosouza <alberto.souza.99@gmail.com>
2016-05-23 23:57:36 -0300

    add test configs to test logs

---
+++ test/testData/config/log.js
@@ -0,0 +1,10 @@
+module.exports = {
+  log: {
+    level: 'warn' ,
+    colorize: true,
+    timestamp: true,
+    prettyPrint: true,
+    depth: 6,
+    showLevel: true
+  }
+}
---
+++ test/testData/invalidLogConfig/config/log.js
@@ -0,0 +1,1 @@
+this will whrow error
