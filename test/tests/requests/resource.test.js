86f4ec17648be1a44a54899c840d62631d020390
albertosouza <alberto.souza.99@gmail.com>
2016-05-23 23:34:25 -0300

    update resource test

--- test/tests/requests/resource.test.js
+++ test/tests/requests/resource.test.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 var request = require('supertest');
 var helpers = require('we-test-tools').helpers;
-var stubs = require('we-test-tools').stubs;
 var Chance = require('chance');
 var chance = new Chance();
 var _, http, we;
@@ -56,6 +55,88 @@ describe('resourceRequests', function() {
             assert.equal(res.body.meta.count, 3);
 
             done();
+          });
+        }).catch(done);
+      });
+    });
+
+    describe('GET /post/:id', function(){
+      it ('should get one post', function (done) {
+        we.db.models.post.create(postStub())
+        .then(function (p) {
+          request(http)
+          .get('/post/'+p.id)
+          .expect(200)
+          .end(function (err, res) {
+            if (err) throw err;
+
+            assert(res.body.post.id);
+            assert.equal(res.body.post.title, p.title);
+            assert.equal(res.body.post.text, p.text);
+
+            done();
+          });
+        }).catch(done);
+      });
+
+      it ('should return 404 to not found', function (done) {
+        var info = we.log.info;
+        we.log.info = function() {};
+        request(http)
+        .get('/post/12321313123121311231231233')
+        .expect(404)
+        .end(function (err, res) {
+          if (err) throw err;
+
+          assert(!res.body.post);
+          we.log.info = info;
+          done();
+        });
+      });
+    });
+
+    describe('PUT /post/:id', function(){
+      it ('should update one post attr', function (done) {
+        we.db.models.post.create(postStub())
+        .then(function (p) {
+
+          var updateData = {
+            title: 'iIIeeei'
+          };
+          request(http)
+          .put('/post/'+p.id)
+          .send(updateData)
+          .expect(200)
+          .end(function (err, res) {
+            if (err) throw err;
+
+            assert(res.body.post.id);
+            assert.equal(res.body.post.title, updateData.title);
+            assert.equal(res.body.post.text, p.text);
+
+            done();
+          });
+        }).catch(done);
+      });
+    });
+
+    describe('DELETE /post/:id', function(){
+      it ('should delete one post', function (done) {
+        we.db.models.post.create(postStub())
+        .then(function (p) {
+          request(http)
+          .delete('/post/'+p.id)
+          .expect(204)
+          .end(function (err, res) {
+            if (err) throw err;
+
+            assert(!res.text);
+
+            we.db.models.post.findById(p.id)
+            .then(function(ps){
+              assert(!ps);
+              done();
+            }).catch(done);
           });
         }).catch(done);
       });
