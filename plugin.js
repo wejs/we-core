93503f85ff69959a520cdfed90d321bfd177e229
albertosouza <alberto.souza.99@gmail.com>
2016-06-25 03:44:12 -0300

    update default database configs to use process enviroment configs

--- plugin.js
+++ plugin.js
@@ -100,26 +100,37 @@ module.exports = function loadPlugin (projectPath, Plugin) {
     database: {
       resetAllData: false,
       prod: {
-        dialect: 'mysql',
-        database: 'test',
-        username: 'root',
-        password: '',
+        uri: process.env.DATABASE_URL,
+        dialect: process.env.DB_DIALECT,
+        protocol: process.env.DB_PROTOCOL,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         // by default log to info
         logging: plugin.we.log.debug
       },
       dev: {
-        dialect: 'mysql',
-        database: 'test',
-        username: 'root',
-        password: '',
+        uri: process.env.DATABASE_URL,
+        dialect: process.env.DB_DIALECT,
+        protocol: process.env.DB_PROTOCOL,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
           // by default log to info
         logging: plugin.we.log.debug
       },
       test: {
-        dialect: 'mysql',
-        database: 'test',
-        username: 'root',
-        password: '',
+        uri: process.env.TEST_DATABASE_URL,
+        dialect: process.env.TEST_DB_DIALECT,
+        protocol: process.env.TEST_DB_PROTOCOL,
+        database: process.env.TEST_DB_NAME,
+        username: process.env.TEST_DB_USER,
+        password: process.env.TEST_DB_PASSWORD,
+        port: process.env.TEST_DB_PORT,
         // by default log to info
         logging: plugin.we.log.debug
       }
