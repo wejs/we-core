2fb01b1266104e3c3c9794c5a085ef0f18f65aa4
albertosouza <alberto.souza.99@gmail.com>
2016-07-30 02:05:43 -0300

    remove uneed template set in main controller

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -9,7 +9,6 @@ module.exports = {
    * Index page route /
    */
   index: function(req, res) {
-    if (!res.locals.template) res.locals.template = 'home/index';
     res.locals.title = null; // dont show duplicated titles
     res.ok();
   }
