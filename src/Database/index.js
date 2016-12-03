c61ed0cf0a8f4fbf8e28c240e5d6ab8648cf79c1
albertosouza <alberto.souza.99@gmail.com>
2016-12-03 19:09:31 -0200

    remove relationships attribute if is empty in JSON api response

--- src/Database/index.js
+++ src/Database/index.js
@@ -156,23 +156,12 @@ function Database (we) {
             relationships: this.getJSONAPIRelationships()
           };
 
-          return formated;
-  //         {
-  //   "type": "articles",
-  //   "id": "1",
-  //   "attributes": {
-  //     "title": "JSON API paints my bikeshed!",
-  //     "body": "The shortest article. Ever.",
-  //     "created": "2015-05-22T14:56:29.000Z",
-  //     "updated": "2015-05-22T14:56:28.000Z"
-  //   },
-  //   "relationships": {
-  //     "author": {
-  //       "data": {"id": "42", "type": "people"}
-  //     }
-  //   }
-  // }
+          // delete the relationships key if is empty
+          if (!Object.keys(formated.relationships).length) {
+            delete formated.relationships;
+          }
 
+          return formated;
         },
 
         /**
