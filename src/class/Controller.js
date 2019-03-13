16e02a305cce61c8d589da0e397f4637717069df
albertosouza <alberto.souza.99@gmail.com>
2019-03-13 19:16:57 -0300

    change updateAttributes to update in default controller

--- src/class/Controller.js
+++ src/class/Controller.js
@@ -123,7 +123,7 @@ Controller.prototype = {
         return res.notFound();
       }
 
-      record.updateAttributes(req.body)
+      record.update(req.body)
       .then(function reloadAssocs(n) {
         return n.reload()
         .then(function() {
